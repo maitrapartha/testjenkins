4162b18eb1023a2307cc73b3bc9b5c8df1d5fba3
parmaitr <partha@publicissapient.com>
2021-03-18 14:21:31 +0530

    Terraform2 trial

--- terraforms/vpc.tf
+++ terraforms/vpc.tf
@@ -62,9 +62,9 @@ resource "aws_security_group" "demo2-public-security-group" {
   }
   egress {
     # allow all traffic to private SN
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"]
   }
