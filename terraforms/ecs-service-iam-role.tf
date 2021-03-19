b621f92fd37649be907ff8adeffe45f2d5448fd2
parmaitr <partha@publicissapient.com>
2021-03-19 16:30:57 +0530

    permission

--- terraforms/ecs-service-iam-role.tf
+++ terraforms/ecs-service-iam-role.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "demo2-ecs-service-policy" {
+/*data "aws_iam_policy_document" "demo2-ecs-service-policy" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
@@ -7,13 +7,32 @@ data "aws_iam_policy_document" "demo2-ecs-service-policy" {
     }
     effect = "Allow"
   }
-}
+}*/
 
 resource "aws_iam_role" "demo2-ecs-service-role" {
   name = "demo2-ecs-service-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+/*resource "aws_iam_role" "demo2-ecs-service-role" {
+  name = "demo2-ecs-service-role"
   assume_role_policy = data.aws_iam_policy_document.demo2-ecs-service-policy.json
   path = "/"
-}
+}*/
 
 resource "aws_iam_role_policy_attachment" "demo2-ecs-role-policy-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
