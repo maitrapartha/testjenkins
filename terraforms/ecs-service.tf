9d8cb8c19408361573e26cc3695061d5fbe2cc2a
parmaitr <partha@publicissapient.com>
2021-03-19 17:08:53 +0530

    update ecs-service.tf with the arn

--- terraforms/ecs-service.tf
+++ terraforms/ecs-service.tf
@@ -1,9 +1,9 @@
 resource "aws_ecs_service" "demo2-ecs-service" {
   name = "demo2-ecs-service"
-  iam_role = aws_iam_role.demo2-ecs-service-role.name
+  iam_role = aws_iam_role.demo2-ecs-service-role.arn
 
   cluster = aws_ecs_cluster.demo2-ecs-cluster.id
-  task_definition = aws_ecs_task_definition.demo2_task_definition.family
+  task_definition = aws_ecs_task_definition.demo2_task_definition.arn
   desired_count = 2
 
   load_balancer {
